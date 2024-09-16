2f92446cd229c9b0f8872c65a3bc9d55cc7a55bd
Imran imtiaz <emraanimtiaz.48@gmail.com>
2024-09-16 07:32:23 +0400

    Update checking-storage-engine.sql

--- mysql-queries/change-table-engine-in-mysql/checking-storage-engine.sql
+++ mysql-queries/change-table-engine-in-mysql/checking-storage-engine.sql
@@ -1,9 +1,11 @@
+-- Query to retrieve the storage engine used by the 'Student' table in the 'University' schema
 SELECT 
-    engine
-FROM
-    information_schema.tables
-WHERE
-    table_schema = 'University'
+    engine 
+FROM 
+    information_schema.tables 
+WHERE 
+    table_schema = 'University' 
     AND table_name = 'Student';
 
+-- Command to display detailed status information about the 'Student' table
 SHOW TABLE STATUS LIKE 'Student';
